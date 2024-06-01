3277938af2b275dba2606cc5182694d38f3d15e0
Quang Cường <thanlequangcuong@gmail.com>
2024-06-01 09:44:40 +0700

    Change contract and interface

--- applications/pages/_app.tsx
+++ applications/pages/_app.tsx
@@ -1,16 +1,33 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
-
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const NEXT_PUBLIC_TEMPLATE_CLIENT_ID="e022f97f17106f10718c0b0e2c535290"
+const activeChain = {
+  chainId: 80002,
+  rpc: ["https://rpc-amoy.polygon.technology/"],
+  shortName: "PoS Amoy",
+  slug: "PolygonAmoy",
+  chain: "PosAmoy",
+  name: "Polygon Amoy",
+  testnet: true,
+  explorers: [
+    {
+      name: "blocksCount",
+      url: "https://www.oklink.com/amoy‍/",
+      standard: "EIP3091",
+    },
+  ],
+  nativeCurrency: {
+    name: "Matic",
+    symbol: "MATIC",
+    decimals: 18,
+  },
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+      clientId={NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
       activeChain={activeChain}
     >
       <Component {...pageProps} />
